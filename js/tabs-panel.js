8a1424d216202de8ee7e3d1ff4238acb737cc158
Nick Nedev <nnedev@aquent.com>
2021-10-16 15:39:19 -0700

    WEB-9589: Updates nav and dropdown Wires nav to reflect value changes in dropdown and vice versa.

--- js/tabs-panel.js
+++ js/tabs-panel.js
@@ -1,21 +1,86 @@
+/**
+ * Tabs Panel
+ * @param {String} navbarContainer
+ * @param {string} tabsContainer
+ *
+ */
 export default class TabsPanel {
-  constructor(tabsPanel){
-    if (tabsPanel) {
-      this.tabsPanel = tabsPanel;
-      
-      const tabPannels = this.tabsPanel.querySelectorAll(".tabs-panel__ul__li")
-      const tabSelect = this.tabsPanel.querySelectorAll(".tabs-panel__ul__li")
-      console.log(panels)
-      tabsPanel.addEventListener("click", this.onClick);
+  constructor(navbarContainer, tabsContainer) {
+    this.tabsContainer = document.querySelector(tabsContainer);
+    this.navbarContainer = document.querySelector(navbarContainer);
+
+    // If there is a navbarContainer and a tabsContainer continue initializing the constructor
+    if (this.tabsContainer && this.navbarContainer) {
+      // the first child of navbarContainer is the mobile dropdown, the second ins the desktop nav
+      this.dropdownMobile = this.navbarContainer.children[0];
+      this.navDesktop = this.navbarContainer.children[1];
+
+      // Get the array of all tabs
+      this.menuItems = this.navDesktop.children;
+      this.dropdownItems = this.dropdownMobile.children;
+      this.tabs = this.tabsContainer.children;
+
+      // Attach event listeners to the desktop navigation bar and the mobile dropdown
+      this.navDesktop.addEventListener("click", this.onClick, false);
+      this.dropdownMobile.addEventListener("change", this.onChange, false);
+
+      this.currentItem = 0;
+      this.prevItem = 0;
+      this.updateDropdown(3)
     }
-
-
   }
+  // onClick for navbar
+  onClick = (e) => {
+    // Due to the 
+    const clickedItem =
+      e.target.nodeName === "A" 
+        ? e.target.parentElement 
+        : e.target;
 
-  onClick(){
-    console.log("clicked");
-    console.log(this.panels)
-  }
+    console.log("clickedItem", clickedItem);
+    const clickedItemNumber = clickedItem.dataset.panelId;
 
+    this.updateDropdown(clickedItemNumber);
+    this.updateNav(clickedItemNumber);
 
+    this.prevItem = this.currentItem;
+    this.currentItem = clickedItemNumber;
+  };
+
+  // onChange for me mobile dropdown
+  onChange = (e) => {
+    const changedItem = e.target.selectedIndex;
+    // console.log("selected index", this.dropdownMobile.selectedIndex)
+
+    this.updateDropdown(changedItem);
+    this.updateNav(changedItem);
+
+    this.prevItem = this.currentItem;
+    this.currentItem = changedItem;
+    // this.updateNav()
+  };
+
+  updateNav = (newMenuItem) => {
+    this.menuItems[this.currentItem].classList.remove("open");
+    this.menuItems[newMenuItem].classList.add("open");
+    this.currentItem = newMenuItem;
+  };
+
+  updateDropdown = (newOptionIndex) => {
+    this.dropdownMobile.options[newOptionIndex].selected = true
+    // console.log("here", newOptionIndex);
+    // this.dropdownMobile.options
+    // console.log();
+  };
+
+  updatePanels = (panelNum) => {};
+
+  /**
+   * Takes in an id containing a number (panel-3) and returns the number
+   * @param {String} str
+   * @returns {String}
+   */
+  getNumericValue = (str) => {
+    return str.replace(/[^0-9]/g, "");
+  };
 }
