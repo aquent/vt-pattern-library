c39d122c2c4b4b2676a7e6a25c1da5673008a1e6
Zoey Le <18235400+zoeyleqa@users.noreply.github.com>
2021-11-29 02:52:19 -0500

    WEB-9660: filter bar adjustment

--- js/filter-bar.js
+++ js/filter-bar.js
@@ -14,8 +14,27 @@ export default class FilterBar extends Menu {
   }
 
   spreadMenuDropdown = () => {
-    // open all details on mobile view
     if (this.navbar) {
+      // open all details on mobile view
+      this.menus.forEach((thisDetail) => {
+        thisDetail.open = true;
+      });
+    }
+  };
+
+  handleSorterChange = () => {
+    document
+      .querySelectorAll(".filter-bar__sorter-input-toggle")
+      .forEach((e) => {
+        e.addEventListener("change", (element) => {
+          document.getElementsByTagName("form")[0].submit();
+          
+        });
+      });
+  };
+
+  handleMobileChange = (e) => {
+    if (e.matches) {
       // Stop closing openthis.menus if the user clicks on a non-menu item
       document.removeEventListener("pointerdown", super.handleNonMenuClick);
 
@@ -23,23 +42,13 @@ export default class FilterBar extends Menu {
       document.removeEventListener("keydown", super.handleEscapeKey);
 
       // Open all filter menu on mobile
-      this.menus.forEach((thisDetail) => {
-        thisDetail.open = true;
-        console.log("detail " + thisDetail.open);
-      });
-    }
-  };
-
-  handleMobileChange = (e) => {
-    if (e.matches) {
-      console.log("spread menu for mobile");
       this.spreadMenuDropdown(this.navbar, this.menus);
     } else {
-      console.log("removing open attr");
       this.menus.forEach((thisDetail) => {
         if (thisDetail.open) thisDetail.removeAttribute("open");
       });
       this.asyncDropdown(this.navbar);
+      this.handleSorterChange();
     }
   };
 
