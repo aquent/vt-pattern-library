f2b52a3604b0e8d33b0556cc0991d7bfcf92b9fb
Nick Nedev <nnedev@aquent.com>
2021-12-07 19:35:53 -0800

    Formats index.js

--- index.js
+++ index.js
@@ -1,41 +1,30 @@
 import "./scss/styles.scss";
 import Menu from "menu";
-import ContenSlider from "content-slider";
+import ContentSlider from "content-slider";
 import VideoController from "video-controller";
-import TabsPanel from "./js/tabs-panel";
+import TabsPanel from "tabs-panel";
 import FilterBar from "filter-bar";
 
-function run () {
-  const menu = new Menu(".navbar");
-  const contentSlider = new ContenSlider(".content-slider", ".content-block__upper");
-  const tabsPanel = new TabsPanel(".tabs-nav", ".tabs");
-  const videoController = new VideoController(
-    ".hero-video__controller-wrapper",
-    ".hero-video__controller"
-  );
-  
-  if (document.querySelector(".filter-bar__menu")) {
-    const filterBar = new FilterBar(".filter-bar__menu");
-    filterBar.controlMenuDropdown();
-  }
-  menu.asyncDropdown();
+const menu = new Menu(".navbar");
+
+const contentSlider = new ContentSlider(
+  ".content-slider",
+  ".content-block__upper"
+);
+
+const tabsPanel = new TabsPanel(
+  ".tabs-nav", 
+  ".tabs"
+);
+
+const videoController = new VideoController(
+  ".hero-video__controller-wrapper",
+  ".hero-video__controller"
+);
+
+if (document.querySelector(".filter-bar__menu")) {
+  const filterBar = new FilterBar(".filter-bar__menu");
+  filterBar.controlMenuDropdown();
 }
 
-
-
-
-
-
-
-// const run = () => {
-//   // const width = window.innerWidth;
-//   // const height = window.innerHeight;
-// const contentSlider = new ContenSlider(".content-slider", ".content-block__upper");
-
-  // const chartNode = document.querySelector("main");
-  // drawChart.default(chartNode, data, width, height);
-// };
-
-// window.addEventListener('resize', run);
-
-document.addEventListener('DOMContentLoaded', run);
+menu.asyncDropdown();
