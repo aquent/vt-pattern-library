324bfeab15d6b18136f7a402c6b153d9714f3995
Nick Nedev <nnedev@aquent.com>
2021-12-28 14:33:57 -0800

    WEB-9724: Disables scroll when mobile menu is open

--- js/menu.js
+++ js/menu.js
@@ -5,12 +5,6 @@ export default class Menu {
   constructor(navbarSelector) {
     this.navbar = document.querySelector(navbarSelector);
     this.menus = this.navbar ? this.navbar.querySelectorAll("details") : null;
-
-    this.menuToggle = this.navbar.nextElementSibling; // The mobile menu toggle button
-    this.documentBody = document.querySelector("body"); // The document body
-
-
-    
   }
 
   handleNonMenuClick = (e) => {
@@ -40,46 +34,29 @@ export default class Menu {
       // Close an open menu if another menu item is opened
       this.menus.forEach((thisDetail, _, details) => {
         thisDetail.ontoggle = (_) => {
-          if (thisDetail.open)
+          if (thisDetail.open) {
             details.forEach((thatDetail) => {
               if (thatDetail != thisDetail) thatDetail.removeAttribute("open");
             });
+          }
         };
       });
 
       document.addEventListener("pointerdown", this.handleNonMenuClick);
       document.addEventListener("keydown", this.handleEscapeKey);
-      this.menuToggle.addEventListener("pointerdown", this.handleScrollAndOverlay);
+      window.addEventListener("hashchange", this.handleScrollAndOverlay);
     }
   };
 
   handleScrollAndOverlay = (e) => {
-    // need the body
-    this.documentBody.classList.toggle("no-scroll");
-    // need the dropdown menu
-    this.navbar.classList.toggle("scroll)");
-    //need the overlay
-
-
-
-    // document.body.classList.toggle("no-scroll");
-    // document.querySelector(".navbar").classList.toggle("scroll");
-    // let dropDownMenu = document.querySelector(".dropdown-menu");
-    // console.log(dropDownMenu)
-    // dropDownMenu.classList.toggle("scroll");
+    if (e.newURL.includes("#navbar")) {
+      document.body.classList.add("no-scroll");
+      this.navbar.classList.add("scroll");
+      return;
+    } else {
+      document.body.classList.remove("no-scroll");
+      this.navbar.classList.remove("scroll");
+      return;
+    }
   }
 }
-
-
-// Notes: Can be an onclick / onpointerdown event handler or use the hashchange event handler. The latter is seems to be more efficient and reliable.
-
-// window.addEventListener('hashchange', function(e){
-//   console.log(e.newURL);
-  
-//   if (e.newURL.includes("navbar")) {
-//     addClass(body, "backdrop");
-//   } else {
-//     removeClass(body, "backdrop");
-//     // addClass(navbar, "navbar-open");
-//   }
-//   })
